02b80e12ca70952a1d7627df35bed0d7f5aebbb3
Brian Ng <bcn220@gmail.com>
2015-08-07 16:00:49 -0600

    refactored using Express

--- server.js
+++ server.js
@@ -1,64 +1,107 @@
-var http = require('http');
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
 
-//this is your resource
+//any request that comes in, look at what comes in if it is JSON convert the req to req.body
+app.use(bodyParser.json());
+
+app.use(function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'OPTIONS, GET, POST');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+});
+
 var messages = [{message: 'hello world'}];
 
-var port = 8887;
+app.get('/', function(req, res) {
+    res.send(JSON.stringify(messages));
+});
 
-var onRequest = function(req, res) {
+app.post('/', function(req, res) {
+    var newMessage = req.body;
+    messages.push(newMessage);
+    res.send(messages);
+});
 
-    //code for GET method
-    if (req.method === 'GET') {
-        res.statusCode = 200;
-
-        res.setHeader('Content-Type', 'application/json');
-
-        // Allow any website to access this API
-        res.setHeader('Access-Control-Allow-Origin', '*');
-        res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET, POST');
-        res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-
-        // Don’t allow scripts or iframes execution from domains we don't trust
-        res.setHeader('X-XSS-Protection', '1; mode=block');
-        res.setHeader('X-Frame-Options', 'SAMEORIGIN');
-        res.setHeader('Content-Security-Policy', "default-src 'self' devmountain.github.io");
-        res.end(JSON.stringify(messages));
-
-    //code for POST method
-    } else if (req.method == 'POST') {
-       var postData = '';
-       //this is just how the web sends data over the web
-       req.on('data', function(chunk) {
-           var chunkie = toString(chunk);
-           console.log(chunkie);
-           postData += chunk.toString();
-        });    
-        req.on('end', function() {
-            console.log("Got POST data:");
-            console.log(JSON.parse(postData));
-            var msg = JSON.parse(postData.toString());
-            messages.push(msg);
-            res.writeHead(200, {
-            'Connection': 'close',
-            'Content-Type': 'application/json',
-            'Access-Control-Allow-Origin': '*',
-            'Access-Control-Allow-Methods': 'OPTIONS, GET, POST',
-            'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept'
-            });
-            res.end(JSON.stringify(messages));
-       });
+app.put('/api/person/:id', function(req, res){
+    var name = req.params.id;
+    res.send("your id is " + name);
+});
 
 
-    } else if (req.method === 'OPTIONS') {
-        res.setHeader('Access-Control-Allow-Origin', '*');
-        res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET, POST');
-        res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-        res.end();
-    }
-};
-
-var server = http.createServer(onRequest).listen(port);
-
-console.log('I\'m watching you...', server.address());
+app.listen(8887);
 
 
+// Example of a body we're getting
+// {
+//     "message":"we are winners"
+// }
+
+
+
+// var http = require('http');
+
+// //this is your resource
+// var messages = [{message: 'hello world'}];
+
+// var port = 8887;
+
+
+// var app = require('./app').create();
+
+// var onRequest = function(req, res) {
+
+//     //code for GET method
+//     if (req.method === 'GET') {
+//         res.statusCode = 200;
+
+//         res.setHeader('Content-Type', 'application/json');
+
+//         // Allow any website to access this API
+//         res.setHeader('Access-Control-Allow-Origin', '*');
+//         res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET, POST');
+//         res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+
+//         // Don’t allow scripts or iframes execution from domains we don't trust
+//         res.setHeader('X-XSS-Protection', '1; mode=block');
+//         res.setHeader('X-Frame-Options', 'SAMEORIGIN');
+//         res.setHeader('Content-Security-Policy', "default-src 'self' devmountain.github.io");
+//         res.end(JSON.stringify(messages));
+
+//     //code for POST method
+//     } else if (req.method == 'POST') {
+//        var postData = '';
+//        //this is just how the web sends data over the web; use the toString method on the data object to store it into vanilla JS 
+//        req.on('data', function(chunk) {
+//            var chunkie = toString(chunk);
+//            console.log(chunkie);
+//            postData += chunk.toString();
+//         });    
+//         req.on('end', function() {
+//             console.log("Got POST data:");
+//             console.log(JSON.parse(postData));
+//             var msg = JSON.parse(postData.toString());
+//             messages.push(msg);
+//             res.writeHead(200, {
+//             'Connection': 'close',
+//             'Content-Type': 'application/json',
+//             'Access-Control-Allow-Origin': '*',
+//             'Access-Control-Allow-Methods': 'OPTIONS, GET, POST',
+//             'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept'
+//             });
+//             res.end(JSON.stringify(messages));
+//        });
+
+
+//     } else if (req.method === 'OPTIONS') {
+//         res.setHeader('Access-Control-Allow-Origin', '*');
+//         res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET, POST');
+//         res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+//         res.end();
+//     }
+// };
+
+// var server = http.createServer(onRequest).listen(port);
+
+// console.log('I\'m watching you...', server.address());
